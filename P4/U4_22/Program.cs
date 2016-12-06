0bfa50c4a205f4c1e14620e4305b5ad15c5cc565
ArminasTomasevicius <arminas.tomasevicius@gmail.com>
2016-12-06 16:15:40 +0200

    Class Work
    Structure

--- P4/U4_22/Program.cs
+++ P4/U4_22/Program.cs
@@ -66,22 +66,60 @@ namespace U4_22
         }
     }
 
+    class Destytojas
+    {
+        private string mpavadinimas, dpavarde, dvardas, grupe, svardas, spavarde;
+        private double credit;
+
+        public Destytojas(string mpavadinimas, string dpavarde, string dvardas, double credit)
+        {
+            this.mpavadinimas = mpavadinimas;
+            this.dpavarde = dpavarde;
+            this.dvardas = dvardas;
+            this.credit = credit;
+        }
+
+        public string MPavadinimas
+        {
+            set { mpavadinimas = value; }
+            get { return mpavadinimas; }
+        }
+
+        public string Dpavarde
+        {
+            set { dpavarde = value; }
+            get { return dpavarde; }
+        }
+
+        public string Dvardas
+        {
+            set { dvardas = value; }
+            get { return dvardas; }
+        }
+
+        public double Credit
+        {
+            set { credit = value; }
+            get { return credit; }
+        }
+    }
+
     class Destytojai
     {
         private int CMaxi = 100;
-        private Modulis[] moduliai;
+        private Destytojas[] destytojai;
         private int n;
         private string pav;
 
         public Destytojai()
         {
             n = 0;
-            moduliai = new Modulis[CMaxi];
+            destytojai = new Destytojas[CMaxi];
         }
 
-        public Studentas Imti(int i)
+        public Destytojai Imti(int i)
         {
-            return moduliai[i];
+            return destytojai[i];
         }
 
         public int Imti()
@@ -89,9 +127,9 @@ namespace U4_22
             return n;
         }
 
-        public void Deti(Studentas ob)
+        public void Deti(Destytojas ob)
         {
-            moduliai[n++] = ob;
+            destytojai[n++] = ob;
         }
     }
 
@@ -105,12 +143,12 @@ namespace U4_22
         public Moduliai()
         {
             n = 0;
-            modulia = new Modulis[CMaxi];
+            studentai = new Studentas[CMaxi];
         }
 
         public Studentas Imti(int i)
         {
-            return moduliai[i];
+            return studentai[i];
         }
 
         public int Imti()
@@ -120,44 +158,107 @@ namespace U4_22
 
         public void Deti(Studentas ob)
         {
-            moduliai[n++] = ob;
+            studentai[n++] = ob;
         }
     }
+
+    class Grupe
+    {
+        const int CMaxi = 100;
+        private Studentas[] Studentai;
+        private int n;
+        private string pav;
+
+        public Grupe()
+        {
+            n = 0;
+            Studentai = new Studentas[CMaxi];
+        }
+
+        public Studentas Imti(int i)
+        {
+            return Studentai[i];
+        }
+
+        public int Imti()
+        {
+            return n;
+        }
+
+        public void Deti(Studentas ob)
+        {
+            Studentai[n++] = ob;
+        }
+    }
+
 
     class Program
     {
         static void Main(string[] args)
         {
+            int n = 0;
+            const string CFd = "...\\...\\Duom.txt";
+            Grupe grupele = new Grupe();
+            Destytojai dest = new Destytojai();
+            Moduliai mod = new Moduliai();
+
+            Skaityti(ref n, CFd, ref grupele, ref mod);
+
+            double suma = 0;
+            int kiekis = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                int poz = 0;
+
+                suma += grupele.Imti(i).Credit;
+
+                for (int j = 0; j < grupele.Imti(); j++)
+                {
+
+                    if (grupele.Imti(j).Pavadinimas == grupele.Imti(i).Grupe)
+                    {
+                        grupele.Imti(j).Kiekis++;
+                        Console.WriteLine(grupele.Imti(j).Kiekis);
+                        grupele.Imti(j).Suma += suma;
+                        poz++;
+                    }
+                }
+
+                if (poz == 0)
+                {
+                    grupele ob = new grupele(grupele.Imti(i).Grupe, kiekis, suma);
+                    grupele.Deti(ob);
+                    Console.WriteLine("Create");
+                }
+            }
 
         }
 
-        static void Skaityti(ref int n, string fv, ref Grupe grupele)
+        static void Skaityti(ref int n, string fv, ref Grupe grupele, ref Moduliai mod)
         {
-           string mpavadinimas, dpavarde, dvardas, grupe, svardas, spavarde;
-           double credit;
+            string mpavadinimas, dpavarde, dvardas, grupe, svardas, spavarde;
+            double credit;
 
-        string line;
-        /*    using (StreamReader reader = new StreamReader(fv))
-            { */
-                while ((line = File.ReadLine()) != null)
-                {
-                    pavadinimas = reader.ReadLine();
+            string line;
+            using (StreamReader reader = new StreamReader(fv))
+            {
                 n = int.Parse(reader.ReadLine());
                 for (int i = 0; i < n; i++)
                 {
                     line = reader.ReadLine();
                     string[] parts = line.Split(' ');
-                    vardas = parts[0];
-                    pavarde = parts[1];
-                    grupe = parts[2];
-                    pkiekis = int.Parse(parts[3]);
+                    mpavadinimas = parts[0];
+                    dpavarde = parts[1];
+                    dvardas = parts[2];
+                    credit = double.Parse(parts[3]);
+                    spavarde = parts[4];
+                    svardas = parts[5];
+                    grupe = parts[6];
 
-                    for (int j = 0; j < pkiekis; j++)
-                    {
-                        paz[j] = int.Parse(parts[j + 3]);
-                    }
                     Studentas ob = new Studentas(mpavadinimas, dpavarde, dvardas, credit, spavarde, svardas, grupe);
                     grupele.Deti(ob);
+                   // mod.Deti(ob);
                 }
 
             }
