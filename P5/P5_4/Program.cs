0261b494e5214153623b1c2ee979b5e26a85f8ad
ArminasTomasevicius <arminas.tomasevicius@gmail.com>
2017-01-08 23:40:08 +0200

    Update
    Needs fix in P5_4 + P4_22 + P5_5 + P5_22

--- P5/P5_4/Program.cs
+++ P5/P5_4/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace P5_4
@@ -10,6 +12,46 @@ namespace P5_4
     {
         static void Main(string[] args)
         {
+            const string input = "..\\..\\input.txt";
+
+            string text = File.ReadAllText(input);
+            string textrev = Reverse(text);
+
+            string[] words = Arraying(text);
+            string[] wordsrev = Arraying(textrev);
+
+            int counter = 0;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 0; j < wordsrev.Length; j++)
+                {
+                    if ((words[i] == wordsrev[j]) && (words[i].Length == wordsrev[j].Length))
+                    {
+                        counter++;
+                        Console.WriteLine(wordsrev[i]);
+                        Console.WriteLine(wordsrev[j]);
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+
+        public static string[] Arraying(string text)
+        {
+            string[] words = Regex.Matches(text, "\\w+")
+              .OfType<Match>()
+              .Select(m => m.Value)
+              .ToArray();
+
+            return words;
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
         }
     }
 }
