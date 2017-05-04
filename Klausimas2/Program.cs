9aba74bec9e0a6af20d3df569fc54a93879b1d90
ArminasTomasevicius <arminas.tomasevicius@gmail.com>
2017-05-04 08:48:38 +0300

    Last Check
    Fast Commit with Klausimas1,2

---
+++ Klausimas2/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klausimas2
+{
+    class Program
+    {
+        class Matrica
+        {
+            public const int Cn = 50;     // maksimalus eilučių, stulpelių skaičius           
+
+            private int[,] DMas;          // dvimatis sveikų skaičių masyvas
+            public int N { get; set; }    // savybė N: eilučių, stulpelių skaičius            
+
+            public Matrica()
+            {
+                DMas = new int[Cn, Cn];
+                N = 0;               
+            }
+
+            public void Deti(int i, int j, int sk)
+            {
+                DMas[i, j] = sk;
+            }
+
+            public int Imti(int i, int j)
+            {
+                return DMas[i, j];
+            }
+
+            // Užrašykite metodą, kuris suskaičiuoja kvadratinės matricos I srities
+            // maksimalų neigiamą elementą.
+            public void Skaiciuotimax(Matrica matrica, out int ats)
+            {
+                int max = int.MinValue;
+                for (int j = matrica.N / 2 + 1; j < matrica.N; j++)
+                {
+                    for (int i = matrica.N - j - 1; i <= j; i++)
+                    {
+                        Console.WriteLine(matrica.Imti(i, j) + "  " + max);
+                        if  (max == int.MinValue && matrica.Imti(i, j) < 0)
+                             max = matrica.Imti(i, j);
+                            if ((matrica.Imti(i, j) > max && matrica.Imti(i, j) < 0))
+                        {
+                            max = matrica.Imti(i, j);
+                        }
+                    }
+                }
+                ats = max;
+                Console.WriteLine(ats + " a");
+            }
+
+
+
+
+        }
+
+            const string CFd = "..\\..\\Matrica1.txt";
+            const string CFd1 = "..\\..\\Matrica2.txt";
+            const string CFr= "..\\..\\Rez.txt";
+
+           
+
+        static void Main(string[] args)
+        {
+            if (File.Exists(CFr))
+                File.Delete(CFr);
+
+            Matrica Mtr = new Matrica();  // Ikonteineris su dvimačiu masyvu
+            Skaityti(CFd, Mtr);
+            Spausdinti(CFr, Mtr," Pirma matrica" );
+
+            Matrica Mtr1 = new Matrica();  // II konteineris su dvimačiu masyvu
+            Skaityti(CFd, Mtr1);
+            Spausdinti(CFr, Mtr1, " Antra matrica");
+
+            int [] B = new int[Mtr.N];
+            int [] B1 = new int[Mtr1.N];
+
+            // Atlikite visus nurodytus skaičiavimus.
+            int ats, ats1;
+            Mtr.Skaiciuotimax(Mtr, out ats);
+            Mtr1.Skaiciuotimax(Mtr1, out ats1);
+
+            if (ats < 0)
+            {
+                Console.WriteLine("Didžiausia neigiama vertė: {0}", ats);
+            }
+            else
+            {
+                Console.WriteLine("Didžiausia neigiama vertė neegzistuoja");
+            }
+
+            if (ats1 < 0)
+            {
+                Console.WriteLine("Didžiausia neigiama vertė: {0}", ats1);
+            }
+            else
+            {
+                Console.WriteLine("Didžiausia neigiama vertė neegzistuoja");
+            }
+
+            Naujas(Mtr, B);
+            Naujas(Mtr1, B1);
+
+            Spausdinti1(CFd, B, Mtr.N, "tekstas");
+            Spausdinti1(CFd, B1, Mtr.N, "tekstas");
+        }
+
+        static void Skaityti(string fv, Matrica A)
+        {
+            using (StreamReader reader = new StreamReader(fv))
+            {
+                int skaicius;
+                string line = reader.ReadLine();
+                char[] skyr = { ' ' };
+                string[] skaiciai = line.Split(skyr,
+                                      StringSplitOptions.RemoveEmptyEntries);
+                A.N = int.Parse(skaiciai[0]);               
+                for (int i = 0; i < A.N; i++)
+                {
+                    line = reader.ReadLine();
+                    skaiciai = line.Split(skyr,
+                                      StringSplitOptions.RemoveEmptyEntries);
+                    for (int j = 0; j < A.N; j++)
+                    {
+                        skaicius = int.Parse(skaiciai[j]);
+                        A.Deti(i, j, skaicius);
+                    }
+                }
+            }
+        }
+
+
+        // Matricos konteinerio duomenų spausdinimas faile fv
+        static void Spausdinti(string fv, Matrica A, string tekstas)
+        {
+            using (var fr = File.AppendText(fv))
+            {
+                fr.WriteLine();
+                fr.WriteLine("      " + tekstas);
+                
+                for (int i = 0; i < A.N; i++)
+                {
+                    for (int j = 0; j < A.N; j++)
+                    {
+                        fr.Write("{0, 4:d}", A.Imti(i, j));
+                    }
+                    fr.WriteLine();
+                }
+            }
+        }
+
+        // Masyvo duomenų spausdinimas faile fv
+        static void Spausdinti1(string fv, int [] B, int n, string tekstas)
+        {
+            using (var fr = File.AppendText(fv))
+            {
+                fr.WriteLine();
+                fr.WriteLine("      " + tekstas);
+
+                for (int i = 0; i < n; i++)
+                {                 
+                    fr.Write("{0, 4:d}", B[i]);                    
+                }
+                fr.WriteLine();
+            }
+        }
+
+
+        // Užrašykite metodą, kuris randa kiekvieno stulpelio antrą didžiausią elementą
+        // ir jį įrašo į naują rinkinį.
+
+        static void Naujas(Matrica matrica, int[] B)
+        {
+            int min = int.MinValue;
+            int min2 = int.MinValue;
+            for (int i = 0; i < matrica.N; i++)
+            {
+                int index =0;
+                for (int j = 0; j < matrica.N; j++)
+                {
+                    if (min < matrica.Imti(i, j))
+                    {
+                        min = matrica.Imti(i, j);
+                        index = j;
+                    }
+                }
+                min = int.MinValue;
+
+                for (int j = 0; j < matrica.N; j++)
+                {
+                    if (j != index && min < matrica.Imti(i, j))
+                    {
+                        min2 = matrica.Imti(i, j);
+                    }
+                }
+
+                B[i] = min2;
+            }
+        }
+
+    }
+}
--- P6/P6_22/Program.cs
+++ P6/P6_22/Program.cs
@@ -10,21 +10,25 @@ namespace P6_22
     class Bankas
     {
         private string pav, salis;
-        private int skaicius;
+        private int nr, skaicius;
 
         public Bankas()
         {
+            nr = 0;
             pav = "";
             salis = "";
             skaicius = 0;
         }
 
-        public void Dėti(string pav, string salis, int skaicius)
+        public void Dėti(int nr, string pav, string salis, int skaicius)
         {
+            this.nr = nr;
             this.pav = pav;
             this.salis = salis;
             this.skaicius = skaicius;
         }
+
+        public int ImtiNr() { return nr; }
 
         public string ImtiPav() { return pav; }
 
@@ -37,6 +41,19 @@ namespace P6_22
             string eilute;
             eilute = string.Format("{0, -15} {1, -10}   {2,2:d}", pav, salis, skaicius);
             return eilute;
+        }
+        
+       public static bool operator >=(Bankas b1, Bankas b2)
+        {
+ 
+            int p = string.Compare(b1.ImtiPav(), b2.ImtiPav(), StringComparison.CurrentCulture);
+            return (p>0);
+        }
+
+        public static bool operator <=(Bankas b1, Bankas b2)
+        {
+            int p = string.Compare(b1.ImtiPav(), b2.ImtiPav(), StringComparison.CurrentCulture);
+            return (p < 0);
         }
 
     }
@@ -69,12 +86,39 @@ namespace P6_22
         public override string ToString()
         {
             string eilute = "";
-            //alternative
             return eilute;
         }
 
         public void Rikiuoti()
         {
+            int temp = 0;
+            for (int write = 0; write < n; write++)
+            {
+                for (int sort = 0; sort < n - 1; sort++)
+                {
+
+                    int b1 = ImtiMatrica(Banku[sort].ImtiNr(), 3);
+                    int b2 = ImtiMatrica(Banku[sort+1].ImtiNr(), 3);
+
+                    if (b1 > b2)
+                    {
+                        temp = b2;
+                        b2 = b1;
+                        b1 = temp;
+                    }
+
+                    if (b1 == b2)
+                    {
+                        Console.WriteLine("A");
+                        if (Banku[sort] >= Banku[sort+1])
+                        {
+                            temp = b2;
+                            b2 = b1;
+                            b1 = temp;
+                        }
+                    }
+                }
+            }
 
         }
 
@@ -91,12 +135,19 @@ namespace P6_22
             if (File.Exists(output))
                 File.Delete(output);
 
-            string laik;
             Bankai bankai = new Bankai();
             SkaitytiMok(input, ref bankai);
-            laik = Console.ReadLine();
-            KiekValstybiu(bankai);
             PradiniaiDuomenys(bankai);
+            KiekValstybiu(bankai);
+            Apsimoka(bankai, 0);
+            Apsimoka(bankai, 1);
+            Apsimoka(bankai, 2);
+            Apsimoka(bankai, 3);
+            Apsimoka(bankai, 4);
+            Apsimoka(bankai, 5);
+            Apsimoka(bankai, 6);
+            RikiuotoSpausdinimas(ref bankai);
+            
         }
 
         static void SkaitytiMok(string fd, ref Bankai bankai)
@@ -118,17 +169,16 @@ namespace P6_22
                     skaicius = int.Parse(parts[2]);
                     Bankas ban;
                     ban = new Bankas();
-                    ban.Dėti(pav, salis, skaicius);
+                    ban.Dėti(i, pav, salis, skaicius);
                     bankai.Dėti(ban);
-                    Console.WriteLine(bankai.n);
                 }
 
                 for (int i = 0; i < bankai.n; i++)
                 {
                     line = reader.ReadLine();
-                    Console.WriteLine(line);
                     parts = line.Split(';');
-                    for (int j = 0; j < 6; j++)
+                    Console.WriteLine(line);
+                    for (int j = 0; j < 7; j++)
                     {
                         prc = int.Parse(parts[j]);
                         bankai.DėtiMatrica(i, j, prc);
@@ -143,36 +193,43 @@ namespace P6_22
             for (int i = 0; i < bankai.n; i++)
             {
                 if (list.Contains(bankai.Imti(i).ImtiSalis()) == false || list.Count == 0)
+                {
                     list.Add(bankai.Imti(i).ImtiSalis());
+                }
             }
-
-            Console.WriteLine(list.Count);
+            using (var fr = File.AppendText(output))
+            {
+                fr.WriteLine();
+                fr.WriteLine("Lietuvoje veikia {0} kitų valstybių bankai", list.Count);
+            }
         }
 
         static void Apsimoka(Bankai bankai, int laik)
         {
             int max = 0;
-            for (int i = 0; i < bankai.ImtiMatrica(laik, i); i++)
+            for (int i = 0; i < bankai.n; i++)
             {
-                int current = bankai.ImtiMatrica(laik, i);
+                int current = bankai.ImtiMatrica(i, laik);
                 if (current > max)
                 {
                     max = current;
                 }
             }
 
-            for (int i = 0; i < bankai.ImtiMatrica(laik, i); i++)
+            for (int i = 0; i < bankai.n; i++)
             {
-                if (max == bankai.ImtiMatrica(laik, i))
+                int current = bankai.ImtiMatrica(i, laik);
+                if (max == current)
                 {
-                    Console.WriteLine("Labiausiai apsimoka laikyti {0}", bankai.Imti(i).ImtiPav());
+                    using (var fr = File.AppendText(output))
+                        fr.WriteLine("{0} laikotarpį labiausiai apsimoka laikyti {1} banke", laik+1, bankai.Imti(i).ImtiPav());
                 }
             }
         }
 
         static void PradiniaiDuomenys(Bankai bankai)
         {
-            using (var fr = File.CreateText(output))
+            using (var fr = File.AppendText(output))
             {
                 fr.WriteLine("Pradiniai duomenys: ");
                 fr.WriteLine();
@@ -182,14 +239,26 @@ namespace P6_22
                     fr.WriteLine(bankai.Imti(i).ToString());
                 }
 
+                fr.WriteLine();
+
                 for (int i = 0; i < bankai.n; i++)
                 {
-                    for (int j = 0; j < 7; j++)
+                    for (int j = 0; j < 6; j++)
                     {
                         fr.Write("{0}; ", bankai.ImtiMatrica(i, j));
                     }
                     fr.WriteLine();
                 }
+            }
+        }
+
+        static void RikiuotoSpausdinimas(ref Bankai bankai)
+        {
+            bankai.Rikiuoti();
+
+            for (int i = 0; i < bankai.n; i++)
+            {
+                Console.WriteLine(bankai.Imti(i).ToString());
             }
         }
     }
